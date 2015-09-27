6aebbc9c916cff179e390b5ef826b2ef989bc6a2
SoleilPatio <soleil.patio@gmail.com>
2015-09-28 02:06:00 +0800

    javatest

---
+++ Java/javatest/src/test/java/com/cloudslee/javatest/EightyOneGoodOrIll.java
@@ -0,0 +1,14 @@
+package com.cloudslee.javatest;
+
+public class EightyOneGoodOrIll {
+	int index;
+	String commentary;
+	String goodorbad;
+
+	public EightyOneGoodOrIll(int idx, String comm, String gob) {
+		index = idx;
+		commentary = comm;
+		goodorbad = gob;
+
+	}
+}
---
+++ Java/javatest/src/test/java/com/cloudslee/javatest/EightyOneGoodOrIllTable.java
@@ -0,0 +1,10 @@
+package com.cloudslee.javatest;
+
+public class EightyOneGoodOrIllTable {
+	static public EightyOneGoodOrIll[] record = { 
+			new EightyOneGoodOrIll(1, "一,吉祥運暗示數（代表健全,幸福,名譽等）", "吉"),
+			new EightyOneGoodOrIll(2, "二,次吉祥運暗示數（代表多少有些障礙，但能獲得吉運）：", "吉"),
+
+	};
+
+}
