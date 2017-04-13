b22783a0aae5fdef6452851bbd8e329ed8520633
clouds.lee <clouds.lee@ubuntu>
2017-04-13 06:32:29 -0700

    .......

--- algorithm/cracking_the_coding_interview_5/arrays_and_strings/arrays_and_strings.py
+++ algorithm/cracking_the_coding_interview_5/arrays_and_strings/arrays_and_strings.py
@@ -17,10 +17,35 @@ def is_all_unique(str):
         
     return True    
 
+"""
+1.2 Implement a function void reverse(char* str) in C or C++ which reverses a null-
+terminated string.
+"""
+def reverse_str(string):
+    count = len(string)
+    
+    for i in range(0, count/2):
+        #swap char
+        swap = string[i]
+        string[i] = string[count-1-i]
+        string[count-1-i] = swap
+        
+    print string
+    print "".join(string)
+        
+        
+"""
+1.3 Given two strings, write a method to decide if one is a permutation of the other.
+"""
+
+
+
 
 
 if __name__ == "__main__":
     
     print is_all_unique("abcd efgh")
+    print reverse_str(list("abcdefgh")) #[CLS]: "abcd" is constant  cannot be modified, use list("abcd")
+    
     
     print "Done"
