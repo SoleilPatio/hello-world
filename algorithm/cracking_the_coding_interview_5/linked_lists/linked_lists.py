403296c76dc692ec252893b2f4d5bfe70974710f
clouds.lee <clouds.lee@ubuntu>
2017-04-20 17:38:42 -0700

    .......

--- algorithm/cracking_the_coding_interview_5/linked_lists/linked_lists.py
+++ algorithm/cracking_the_coding_interview_5/linked_lists/linked_lists.py
@@ -1,3 +1,4 @@
+from _random import Random
 
 class Node:
     def __init__(self, data):
@@ -181,12 +182,61 @@ def main_del_node():
 
 """
 2.4 Write code to partition a linked list around a value x, such that all nodes less than
-x come before all nodes greater than or equal to x.
+x come before all nodes greater than or equal to x. 
+
+[CLS][NOTE]:IT'S VERY HARD TO MOVE LINK OF SINGLE LINKED LIST!!!!!!!USE DATA SWAP!!
 """
+import numpy as np
+def gen_test_linked_list_random(count):
+    head = Node(np.random.randint(count))
+    for x in np.random.randint(count,size=count):
+        head.append(Node(x))
+    head.traversal()
+    
+    return head
+
+def gen_test_linked_list_seq(count):
+    head = Node(0)
+    for x in range(1,count):
+        head.append(Node(x))
+    head.traversal()
+    
+    return head
+    
+def partition_list(head, num):
+    rp = head
+    pp = head #point to the end of less than
+    
+    while(rp != None):
+        head.traversal()
+        if rp.m_data < num:
+            if rp != pp:
+                #swap
+                temp = pp.m_data
+                pp.m_data = rp.m_data
+                rp.m_data = temp
+            pp = pp.m_next
+            rp = rp.m_next
+        else:
+            rp = rp.m_next
+            
+    return head
+    
+    
+def main_partition():
+    head = gen_test_linked_list_random(20)
+#     head = gen_test_linked_list_seq(20)
+    head = partition_list(head,10)
+    head.traversal()
+    
+    
+    pass
+
+
 
 """
 2.5 You have two numbers represented by a linked list, where each node contains a
-single digit. The digits are stored in reverse order, such that the Ts digit is at the
+single digit. The digits are stored in reverse order, such that the 1's digit is at the
 head of the list. Write a function that adds the two numbers and returns the sum
 as a linked list.
 EXAMPLE
@@ -198,6 +248,125 @@ EXAMPLE
 Input: (6 -> 1 -> 7) + (2 -> 9 -> 5).That is, 617 + 295.
 Output: 9 -> 1 -> 2.That is, 912.
 """
+def gen_list(d_list):
+    if len(d_list) == 0:
+        return None
+    
+    head = Node(d_list[0])
+    for d in d_list[1:]:
+        head.append(Node(d)) 
+        
+    head.traversal()
+    return head
+
+def sum_list( l1, l2):
+    import math
+    sum_list = None
+    sum = 0
+    carry = 0
+    while( l1 != None or l2 != None or carry != 0):
+        l1d = l1.m_data if l1 != None else 0
+        l2d = l2.m_data if l2 != None else 0
+        
+        sum = l1d + l2d + carry
+        
+        if sum >= 10:
+            carry = 1
+            num = sum-10
+        else:
+            carry = 0
+            num = sum
+
+        if sum_list == None:
+            sum_list = Node(num)
+        else:
+            sum_list.append(Node(num))
+            
+        
+        l1 = l1.m_next if l1 != None else None
+        l2 = l2.m_next if l2 != None else None
+        
+    return sum_list
+    
+    
+def sum_list_reverse(l1,l2,sum):
+    if(l1 == None ):
+        return 0
+     
+    carry = sum_list_reverse(l1.m_next, l2.m_next, sum)
+    
+    l1d = l1.m_data
+    l2d = l2.m_data
+    
+    sumd = l1d + l2d + carry
+    
+    if sumd >= 10:
+        carry = 1
+        num = sumd - 10
+    else:
+        carry = 0
+        num = sumd
+        
+    sum_next = sum.m_next
+    sum.m_next = Node(num)
+    sum.m_next.m_next = sum_next
+    
+    return carry
+        
+    
+     
+    
+    
+    
+def list_len(li):
+    ret_len = 0
+    while(li != None):
+        ret_len += 1
+        li = li.m_next
+    return ret_len
+    
+    
+def pad_list(l1,l2):
+    l1len = list_len(l1)
+    l2len = list_len(l2)
+    
+    if(l1len > l2len):
+        diff = l1len-l2len
+        while(diff > 0):
+            newl2 = Node(0)
+            newl2.m_next = l2
+            l2 = newl2
+            diff -= 1
+    elif(l1len < l2len):
+        diff = l2len-l1len
+        while(diff > 0):
+            newl1 = Node(0)
+            newl1.m_next = l1
+            l1 = newl1
+            diff -= 1
+    
+    return (l1,l2)
+        
+    
+    
+    
+
+def main_sum_list():
+    l1 = gen_list([1,2,6,1,7])
+    l2 = gen_list([2,9,5])
+    
+    sum = sum_list(l1,l2)
+    sum.traversal()
+    
+    print "Part B:Reverse"
+    #Part B: reverse
+    (l1,l2) = pad_list(l1,l2)
+    l1.traversal()
+    l2.traversal()
+    
+    sum = Node(0)
+    sum_list_reverse(l1,l2,sum)
+    sum.traversal()
 
 """
 2.6 Given a circular linked list, implement an algorithm which returns the node at
@@ -209,6 +378,34 @@ EXAMPLE
 Input: A - > B - > C - > D - > E - > C [the same C as earlier]
 Output: C
 """
+
+def check_circular(linklist):
+    nodedict = {}
+    
+    while(linklist != None):
+        if linklist in nodedict:
+            nodedict[linklist] += 1
+            break
+        else:
+            nodedict[linklist] = 1
+        linklist = linklist.m_next
+        
+    for node in nodedict.keys():
+        if nodedict[node] > 1:
+            print "loop start:",node.m_data
+            
+            
+
+def main_check_circular():
+     l1 = gen_list([1,2,6,1,7])
+     
+     node6 = l1.m_next.m_next
+     l1.append(node6)
+     
+     check_circular(l1)
+    
+    
+        
 
 """
 2.7 Implement a function to check if a linked list is a palindrome
@@ -249,8 +446,10 @@ if __name__ == "__main__":
 #     main_remove_duplicate()
 #     main_find_kth_last_element()
 #     main_del_node()
+#     main_partition()
+#     main_sum_list()
+    main_check_circular()
     main_is_polindrome()
-    
     
     
     print "-----Done"
