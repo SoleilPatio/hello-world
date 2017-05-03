7f4127ec905213f5f16fc67639bccd557d321d66
clouds.lee <clouds.lee@ubuntu>
2017-05-03 02:27:14 -0700

    .......

--- algorithm/cracking_the_coding_interview_5/python/mathematics_and_probability.py
+++ algorithm/cracking_the_coding_interview_5/python/mathematics_and_probability.py
@@ -61,6 +61,9 @@ Game 2: You get three shots and you have to make two of three shots.
 If p is the probability of making a particular shot, for which values of p should
 you pick one game or the other
 """
+"""
+p >? 3p^2 - 2p^3
+"""
 
 """
 7.2 There are three ants on different vertices of a triangle. What is the probability of
@@ -90,6 +93,139 @@ parallel to the x-axis.
 7.6 Given a two-dimensional graph with points on it, find a line which passes the
 most number of points.
 """
+THRESHOLD = 0.001
+import math
+class point(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def set(self,x , y):
+        self.x = x
+        self.y = y
+        
+class line(object):
+    def __init__(self, p1, p2):
+        if abs(p2.x - p1.x) >  THRESHOLD:
+            self.slope = (p2.y - p1.y) / (p2.x - p1.x)
+            self.intercept = p1.y - self.slope * p1.x
+        else:
+            self.slope = None #slop == None means vertical line
+            self.intercept = p1.x
+            
+        print "line=",self.slope,self.intercept,p2.x,p1.x,THRESHOLD
+    def __cmp__(self, other):
+        print "i am cmp"
+        if self.slope != None and other.slope != None:
+            if (abs(self.slope - other.slope) < THRESHOLD) and (abs(self.intercept - other.intercept) < THRESHOLD):
+                return 0
+            else:
+                return -1
+        elif self.slope == None and other.slope == None:
+            if (abs(self.intercept - other.intercept) < THRESHOLD):
+                return 0
+            else:
+                return -1
+        return -1
+                
+    def __eq__(self, other):
+        print "i am eq",self.slope,other.slope,self.intercept ,other.intercept
+        if self.slope != None and other.slope != None:
+            if (abs(self.slope - other.slope) < THRESHOLD) and (abs(self.intercept - other.intercept) < THRESHOLD):
+                return True
+            else:
+                return False
+        elif self.slope == None and other.slope == None:
+            if (abs(self.intercept - other.intercept) < THRESHOLD):
+                return True
+            else:
+                return False
+        return False
+        
+            
+class line_hash(object):
+    def __init__(self):
+        self.hash = {}
+        
+    def round_number(self, float_num):
+        if float_num == None:
+            return None
+        q = int(float_num/THRESHOLD)
+        return q*THRESHOLD
+        
+    def add_line_to_hash(self, line):
+        key = self.round_number(line.slope)
+        if key in self.hash:
+            self.hash[key].append(line)
+        else:
+            self.hash[key] = [line]
+        
+    def count_identical_lines_by_slope(self, slope, line):
+        key = self.round_number(slope)
+        count = 0
+        for l in self.hash.get(key,[]):
+            print "compare", line == l
+            if line == l:
+                count += 1
+        return count
+        
+        
+    def count_identical_lines(self,line):
+        if line.slope == None:
+            c1 = self.count_identical_lines_by_slope(line.slope, line)
+            return c1
+        else:
+            c1= self.count_identical_lines_by_slope(line.slope-THRESHOLD, line)
+            c2 = self.count_identical_lines_by_slope(line.slope, line)
+            c3 = self.count_identical_lines_by_slope(line.slope+THRESHOLD, line)
+        
+        print c1,c2,c3
+        return c1+c2+c3
+                
+
+
+
+
+def find_the_best_line(points):
+    linstable = line_hash() #(slope:line list)
+    
+    max_count = 0
+    best_line = None
+    
+    for i , p1 in enumerate(points):
+        for j , p2 in enumerate(points[i+1:]):
+            print "(i,j)=",i,j
+            ll = line(p1,p2)
+            linstable.add_line_to_hash(ll)
+            count = linstable.count_identical_lines(ll)
+            
+            if (count > max_count):
+                best_line = ll
+                max_count = count
+                
+    return best_line, count
+            
+
+
+def main_find_best_line():
+    import numpy as np
+    points = [point(0,0) for _ in range(10)]
+    randoms = np.random.randint(5, size = 20)
+    print randoms
+    for i in range(10):
+        points[i].set(float(randoms[i]), float(randoms[i+1]))
+    
+    for p in points:
+        print p.x, p.y
+    
+    
+    best_line, count = find_the_best_line(points)
+    
+    print "best_line.slope", best_line.slope, "best_line.intercept", best_line.intercept
+    print "count", count
+    
+    
+    
+
 
 """
 7.7 Design an algorithm to find the kth number such that the only prime factors are
@@ -100,9 +236,11 @@ most number of points.
 
 
 if __name__ == "__main__":
-    import timeit
+#     import timeit
+#     
+#     print timeit.timeit("main_gen_primes1()", setup="from __main__ import main_gen_primes1",number=1)
+#     print timeit.timeit("main_gen_primes2()", setup="from __main__ import main_gen_primes2",number=1)
     
-    print timeit.timeit("main_gen_primes1()", setup="from __main__ import main_gen_primes1",number=1)
-    print timeit.timeit("main_gen_primes2()", setup="from __main__ import main_gen_primes2",number=1)
+    main_find_best_line()
     
     print "Done!"
