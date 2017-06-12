305bfcd95c61559cbdde2f466f7ede784c648dbd
clouds@ubuntu <clouds@ubuntu>
2017-06-11 18:59:25 -0700

    .......

--- python/namedtuple_test.py
+++ python/namedtuple_test.py
@@ -5,7 +5,12 @@
 if __name__ =="__main__":
     import collections
     
-    Point = collections.namedtuple("Point",['x', 'y'])
+    """
+    [CLS]:
+        1. Point is a new-created class name
+        2. "Point_data_type_name" is the data type name
+    """
+    Point = collections.namedtuple("Point_data_type_name",['x', 'y'])
     
     p1 = Point(1,2)
     p2 = Point(3,4)
@@ -16,7 +21,9 @@ if __name__ =="__main__":
          [NOTE!!!!]: ONLY FOR CONSTANT
     """
     
-    print p1
+    print p1.x, p1.y
+    print p1[0], p1[1]
+    print "type=",type(p1)
     print p2
     print p3
     
