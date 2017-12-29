77dd9e3da3436c268a6bec18f5728714225d66ee
clouds@ubuntu <clouds@ubuntu>
2017-12-29 00:59:00 -0800

    .......

--- python3/tensorflow/hello.py
+++ python3/tensorflow/hello.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 def hello():
     hello = tf.constant("Hello, TensorFlow")
@@ -155,6 +156,70 @@ def linear_regression():
     
     
     
+def linear_regression_tf_estimator():
+    """
+    Step1: Prepare model & data
+    """
+    """
+    Clouds: 1-D feature, named "x"
+    """
+    feature_columns = [tf.feature_column.numeric_column("x", shape=[1])]
+    
+    print("feature_column =", feature_columns)
+    
+    """
+    a linear regression 
+    Clouds: feed estimator "only the features" , it will automatically decide the dimention of W and b, this is reasonable
+    """
+    """
+    create a estimator with 1-d feature named "x"
+    """
+    estimator = tf.estimator.LinearRegressor(feature_columns = feature_columns)
+    
+    """
+    prepare tensorflow data from numpy
+    Clouds: what is  batch_size? num_epochs? shuffle????
+    """
+    x_train = np.array([1., 2., 3., 4.])
+    y_train = np.array([0., -1., -2., -3.])
+    x_eval = np.array([2., 5., 8., 1.])
+    y_eval = np.array([-1.01, -4.1, -7, 0.])
+    input_fn = tf.estimator.inputs.numpy_input_fn(
+        {"x": x_train}, y_train, batch_size=4, num_epochs=None, shuffle=True)
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        {"x": x_train}, y_train, batch_size=4, num_epochs=1000, shuffle=False)
+    
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+        {"x": x_eval}, y_eval, batch_size=4, num_epochs=1000, shuffle=False)
+    
+    print("input_fn=",input_fn)
+    print("train_input_fn=",train_input_fn)
+    print("eval_input_fn=",eval_input_fn)
+    
+    
+    
+    """
+    Step2: Training with input_fn
+    Clouds: input_fn has 1. sample features (x) 2. result (y)
+    """
+    # We can invoke 1000 training steps by invoking the  method and passing the
+    # training data set.
+    result = estimator.train(input_fn=input_fn, steps=1000)
+    print("estimator.train result=", result)
+    
+    """
+    Step3: Evaluate
+    """
+    # Here we evaluate how well our model did.
+    train_metrics = estimator.evaluate(input_fn=train_input_fn)
+    eval_metrics = estimator.evaluate(input_fn=eval_input_fn)
+    print("train metrics: %r"% train_metrics)
+    print("eval metrics: %r"% eval_metrics)
+     
+    
+    
+    
+    
     
     
     
@@ -209,7 +274,8 @@ if __name__ == '__main__':
 #     test1()
 #     test2() #placeholder test
 #     mytest()
-    linear_regression()
+#     linear_regression()
+    linear_regression_tf_estimator()
     
     print("Done!")
     pass
