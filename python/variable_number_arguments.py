8e52798cf50867e3c81ff8cf1baf3bb34df3a230
clouds@PC <clouds@PC>
2018-11-21 10:19:04 +0800

    .......

--- python/variable_number_arguments.py
+++ python/variable_number_arguments.py
@@ -2,26 +2,42 @@
 def Foo(*arg):
 	print "[CLS]: *arg is a argument tuple : %s" % str(type(arg))
 	print "arg=",arg
+	print ""
 
 def Bar(**kwargs):
 	print "[CLS]: **kwargs is a dict : %s" % str(type(kwargs))
 	print "kwargs=",kwargs
+	print ""
 	
 	
 """
-[CLS]:" **kwargs" must be the last argument
+[CLS]:" **kwargs (keyword argument)" must be the last argument
 """
 def FooBar(*arg, **kwargs):
 	print "[CLS]: *arg is a argument tuple : %s" % str(type(arg))
 	print "arg=",arg
 	print "[CLS]: **kwargs is a dict : %s" % str(type(kwargs))
 	print "kwargs=",kwargs
+	print ""
 
 
 if __name__ == "__main__":
+	a_list = ["a","b","c"]
 	Foo(1,2,3,"a","b","c")
-	Bar(v1=1, v2=2, v3=3, v4="a", v5="b", v6_6="c")
+	"""[CLS]: ** expand a list in un_named arguments"""
+	Foo(1,2,3,*a_list)
+	print "-----------------------"
+	
+	a_dict = {"v1":1, "v2":2, "v3":3 }
+	Bar(v1=1, v2=2, v3=3)
+	"""[CLS]: ** expand a dict in named arguments"""
+	Bar(**a_dict) 
+	print "-----------------------"
+	
+	
 	FooBar(1,2,3,v1=1,v2=2,v3=3)
+	FooBar(*a_list,**a_dict)
+	print "-----------------------"
 	
 	
 	print "\nDone!"
