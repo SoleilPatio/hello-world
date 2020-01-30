99f752c8223def05f3c11a144ecbfe069dc1b0aa
ELPIS <ELPIS@yuntin.com>
2020-01-30 10:35:33 +0800

    check tcp port open or not

--- shellscript/snippet.sh
+++ shellscript/snippet.sh
@@ -28,3 +28,25 @@ function_test()
 }
 
 function_test arg1 arg2
+#---------------------- [network check port]
+(echo > /dev/tcp/localhost/20) > /dev/null 2>&1
+result=$?
+echo "resutl=$result"
+if [ $result -eq 0 ]; then
+    echo "20 port open"
+else
+    echo "20 port closed"
+fi
+
+#---------------------- [network scan port]
+for (( port=630; port<=635; port++ ))
+    do
+        (echo > /dev/tcp/127.0.0.1/$port) > /dev/null 2>&1
+        result=$?
+        echo "resutl=$result"
+        if [ $result -eq 0 ]; then
+            echo "$port open"
+        else
+            echo "$port closed"
+        fi
+    done
