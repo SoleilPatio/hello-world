7bf35f8e225daa5360e87aae2476f3f187113a58
SoleilPatio <soleil.patio@gmail.com>
2020-12-04 09:23:28 +0800

    Update scipy_test.py

--- python/PACKAGE_SAMPLE/scipy_test.py
+++ python/PACKAGE_SAMPLE/scipy_test.py
@@ -6,6 +6,23 @@ import unittest
 from matplotlib import pyplot as plt
 import numpy as np
 
+def interpolate_2D():
+    from scipy import interpolate
+
+    x = [ 50, 60, 70 ]
+    y = [ 2 , 2.5 , 3]
+    z = [
+        [ 1, 2, 3],
+        [ 4, 5, 6],
+        [ 7, 8, 9] 
+    ]
+
+    # f = interpolate.interp2d(x, y, z, kind='cubic')
+    f = interpolate.interp2d(x, y, z, kind='linear', fill_value=None)
+    print(f(50,2))
+    
+
+    
 
 class Test(unittest.TestCase):
 
