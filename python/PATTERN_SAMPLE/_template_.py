d2b80fe3b6a1552395f236079b7f0908f5e49e53
ATHENE <ATHENE@yuntin.com>
2021-03-05 00:46:31 +0800

    single libpython, move to cls-toolbox

--- python/PATTERN_SAMPLE/_template_.py
+++ python/PATTERN_SAMPLE/_template_.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 
+import argparse
+
 try:
     # for Python2
     import Tkinter as tk
@@ -12,5 +14,66 @@ except:
     from tkinter import ttk
 
 
-if __name__ == '__main__':
-    print("中文!Done!")
+
+__version_info__ = ('2013','03','14')
+__version__ = '-'.join(__version_info__)
+
+
+if __name__ == "__main__":
+    
+    """
+    [CLS]: Normal Usage
+    """
+    parser = argparse.ArgumentParser()
+
+
+    """
+    [CLS]: a positional argument (no -- option in front of the name) , always requires
+    """
+    parser.add_argument("square", type=int, help="display a square of a given number")
+    #"--verbose",
+
+
+    """
+    [CLS]: argument
+    """
+    parser.add_argument("-foo", help="info for foo", default="default value")
+    parser.add_argument("-bar", help="info for bar", default="default value", required=True)
+    parser.add_argument("-boolean",  action="store_true", help="increase output verbosity")
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
+    
+    
+    
+    # This is the correct way to handle accepting multiple arguments.
+    # '+' == 1 or more.
+    # '*' == 0 or more.
+    # '?' == 0 or 1.
+    # An int is an explicit number of arguments to accept.
+    """
+    drawback: eating position arguments. ex. foo.py --nargs 111 222 333 position_args_not work
+    """
+    parser.add_argument('--nargs', nargs='+')
+    
+    """
+    use append
+    mycommand --carpark 17 --carpark 18
+    """
+    parser.add_argument('--carpark',
+                    dest='carpark_ids',
+                    type=int,
+                    action='append',
+                    default=[],
+                    help="One carpark ID (can be used multiple times)"
+                    )
+    
+    
+    
+    """
+    [CLS]: if argements are illegal, this "parse_args" will show help and exit program
+    """
+    args = parser.parse_args()
+
+    print("args = ", args)
+    
+    
+    print("Done")
--- python/libPython/core/_libpythonpath_.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from __future__ import print_function
-#................................................................................
-# Add libPython\.. into PYTHONPATH if in unit test
-#................................................................................
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
